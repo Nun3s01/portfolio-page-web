259f7dc9f5332c04f267f153d875b107a9484a16
Cauã Nunes <106976173+Nun3s01@users.noreply.github.com>
2024-09-03 12:00:06 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+## PORTFÓLIO PROFISSIONAL
+
+
+### [Acesse meu portfólio aqui.](https://nun3s01.github.io/portfolio-page-web/)
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+
+>###### Criado por Cauã Nunes Novaes
---
+++ js/animation_container.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function () {
+  const containerMain = document.getElementById('container-main');
+  const scrollPosition = window.scrollY;
+  const windowHeight = window.innerHeight;
+
+  if (scrollPosition > windowHeight / 2) {
+    containerMain.classList.add('visible');
+  } else {
+    containerMain.classList.remove('visible');
+  }
+});
