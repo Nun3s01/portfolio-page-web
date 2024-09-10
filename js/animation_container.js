15677d4c85742ba73860383c3e66739f198063ea
Cauã Nunes <106976173+Nun3s01@users.noreply.github.com>
2024-09-10 22:22:19 +0000

    animation_container.js

--- js/animation_container.js
+++ js/animation_container.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
   const scrollPosition = window.scrollY;
   const windowHeight = window.innerHeight;
 
-  if (scrollPosition > windowHeight / 2.5) {
+  if (scrollPosition > windowHeight / 1.8) {
     containerMain.classList.add('visible');
   } else {
     containerMain.classList.remove('visible');
